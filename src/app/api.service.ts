554fc67f6ecb13e99d3778ec7c86b431e931b93a
Steve Thompson <writetome51@gmail.com>
2018-02-09 09:06:36 -0700

    new commit

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,8 +8,33 @@ export class ApiService {
   }
 
 
-  search() {
-    this._http.get('');
+  search(searchString) {
+    this._http.get(searchString);
   }
 
+  getSpecific() {}
+
 }
+
+/********
+getCharacters(): Observable<Character[]> {
+  return this.httpClient
+    .get<Character[]>(this.characterUrl)
+    .pipe(catchError(this.handleError));
+}
+
+getCharacter(characterId: string): Observable<Character> {
+  return this.httpClient
+    .get<Character[]>(this.characterUrl)
+    .pipe(
+      map((characters: Character[]) => characters.find(character => character.id == characterId)),
+      catchError(this.handleError)
+    )
+}
+
+handleError(error) {
+  console.log(error);
+  return Observable.throw(error.json().error || 'Server error');
+}
+ ******/
+
