031fa7ae8e21b75f378847e529e97359b7d04400
Steve Thompson <writetome51@gmail.com>
2018-02-09 09:29:49 -0700

    changes made to ApiService

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,29 +1,39 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ApiService {
+
+  private _apiKey = '4f5c0e8ca2c5a339763c1a9b0ce2374d';
+  private _baseSearchUrl = 'http://food2fork.com/api/search';
 
   constructor(private _http: HttpClient) {
   }
 
 
-  search(searchString) {
-    this._http.get(searchString);
+  search(recipeSearch): Observable<any> {
+    // spaces in searchString probably be automatically converted
+    // to %20 for us.
+      let getParameters = `?key=${this._apiKey}&q=${recipeSearch}`;
+      let fullUrl = `${this._baseSearchUrl}${getParameters}`;
+      return this._http.get(fullUrl);
   }
 
-  getSpecific() {}
+  getSpecific(recipe): Observable<any> {
+
+  }
 
 }
 
 /********
-getCharacters(): Observable<Character[]> {
+ getCharacters(): Observable<Character[]> {
   return this.httpClient
     .get<Character[]>(this.characterUrl)
     .pipe(catchError(this.handleError));
 }
 
-getCharacter(characterId: string): Observable<Character> {
+ getCharacter(characterId: string): Observable<Character> {
   return this.httpClient
     .get<Character[]>(this.characterUrl)
     .pipe(
@@ -32,7 +42,7 @@ getCharacter(characterId: string): Observable<Character> {
     )
 }
 
-handleError(error) {
+ handleError(error) {
   console.log(error);
   return Observable.throw(error.json().error || 'Server error');
 }
