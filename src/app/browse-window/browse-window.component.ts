867c5350803323e606d776202b48f374fed97a9c
Morrianton1985 <morrianton@gmail.com>
2018-02-14 07:59:47 -0700

    Data binding

--- src/app/browse-window/browse-window.component.ts
+++ src/app/browse-window/browse-window.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchComponent} from '../search/search.component';
 
 @Component({
   selector: 'app-browse-window',
   templateUrl: './browse-window.component.html',
   styleUrls: ['./browse-window.component.css']
 })
-export class BrowseWindowComponent implements OnInit {
+export class BrowseWindowComponent extends SearchComponent  implements OnInit {
+
+  recipeName: string;
+  recipeImage: string;
+  imageDescription: string;
+  cookTime: number;
+  recipeDescription: string;
 
   constructor() { }
 
