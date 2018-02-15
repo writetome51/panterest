875f0d884bdb84405e42abd8aff5106ab608ac22
Morrianton1985 <morrianton@gmail.com>
2018-02-15 07:57:30 -0700

    Added some to browse-window.component.ts

--- src/app/browse-window/browse-window.component.ts
+++ src/app/browse-window/browse-window.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SearchComponent} from '../search/search.component';
 import {ApiService} from '../services/api.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-browse-window',
   templateUrl: './browse-window.component.html',
   styleUrls: ['./browse-window.component.css']
 })
-export class BrowseWindowComponent implements OnInit {
+export class BrowseWindowComponent extends SearchComponent implements OnInit, OnDestroy {
 
   recipeName: string;
   recipeImage: string;
   imageDescription: string;
   cookTime: number;
   recipeDescription: string;
+  searchSubscription: Subscription;
+  result: any;
 
-  constructor() { }
+  constructor(_api: ApiService) {
+    super(_api);
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.searchSubscription.unsubscribe();
   }
 
 }
