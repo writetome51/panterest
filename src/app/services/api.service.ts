f4ed49fd173aa9089b410c87a68ee12356005e89
Steve Thompson <writetome51@gmail.com>
2018-02-16 12:48:30 -0700

    minor changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,12 +9,13 @@ import {Subscription} from 'rxjs/Subscription';
 @Injectable()
 export class ApiService {
 
-  private _apiKey = '4f5c0e8ca2c5a339763c1a9b0ce2374d';
   private _corsProxy = 'https://cors-anywhere.herokuapp.com/';
   private _baseUrl = this._corsProxy + 'http://food2fork.com/api/';
   private _baseSearchUrl = this._baseUrl + 'search';
   private _baseRecipeRequestUrl = this._baseUrl + 'get';
   private _keyParam = 'key';
+  private _apiKey = '4f5c0e8ca2c5a339763c1a9b0ce2374d';
+  private _keyParamValuePair = this._keyParam + '=' + this._apiKey;
   private _queryParam = 'q';
   private _recipeIDParam = 'rId';
   private _pageParam = 'page';
@@ -53,14 +54,14 @@ export class ApiService {
     // spaces in searchString probably be automatically converted
     // to %20 for us.
     let getParameters =
-      `?${this._keyParam}=${this._apiKey}&${this._queryParam}=${recipeSearch}&${this._pageParam}=${this._currentPage}`;
+      `?${this._keyParamValuePair}&${this._queryParam}=${recipeSearch}&${this._pageParam}=${this._currentPage}`;
     let fullUrl = `${this._baseSearchUrl}${getParameters}`;
     return this._http.get(fullUrl, this._httpOptions);
   }
 
 
   private _getSpecificRecipeAsObservable(recipeId): Observable<any> {
-    let getParameters = `?${this._keyParam}=${this._apiKey}&${this._recipeIDParam}=${recipeId}`;
+    let getParameters = `?${this._keyParamValuePair}&${this._recipeIDParam}=${recipeId}`;
     let fullUrl = `${this._baseRecipeRequestUrl}${getParameters}`;
     return this._http.get(fullUrl, this._httpOptions);
   }
