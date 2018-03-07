a3da162d1c68b35f590cf3fb6882e41381b48c61
Steve Thompson <writetome51@gmail.com>
2018-03-07 13:50:08 -0700

    new commit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,30 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Observer} from '../interfaces/Observer';
 import {SearchResultRecipe} from '../interfaces/SearchResultRecipe';
 import {Subscription} from 'rxjs/Subscription';
-import {environment as e} from '../../environments/environment';
 
 @Injectable()
 export class ApiService {
+
+    private _corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    private _baseUrl = this._corsProxy + 'http://food2fork.com/api/';
+    private _baseSearchUrl = this._baseUrl + 'search';
+    private _baseRecipeRequestUrl = this._baseUrl + 'get';
+    private _keyParam = 'key';
+    private _apiKey = '4f5c0e8ca2c5a339763c1a9b0ce2374d';
+    private _keyParamValuePair = this._keyParam + '=' + this._apiKey;
+    private _queryParam = 'q';
+    private _recipeIDParam = 'rId';
+    private _pageParam = 'page';
+    private _httpOptions = {
+        headers: new HttpHeaders({
+            'Authorization': this._apiKey,
+            'Accept': ['text/html', 'application/json'],
+            'apikey': this._apiKey,
+        })
+    };
 
     constructor(private _http: HttpClient) {
     }
@@ -41,7 +58,7 @@ export class ApiService {
 
 
     private _getTopRatedAndGetObservable(resultPage) {
-        let keyValueList = [e.api.pageParam, String(resultPage)]; // alternating key and value;
+        let keyValueList = [this._pageParam, String(resultPage)]; // alternating key and value;
         let getParameters = this._createGetParameters(keyValueList);
         return this._getRequest(
             this._createRequestUrl('search', getParameters)
@@ -52,8 +69,8 @@ export class ApiService {
     private _searchAndGetObservable(recipeSearch, resultPage): Observable<any> {
 
         let getParameters = this._createGetParameters([
-            e.api.queryParam, recipeSearch,
-            e.api.pageParam, String(resultPage)
+            this._queryParam, recipeSearch,
+            this._pageParam, String(resultPage)
         ]);
         return this._getRequest(
             this._createRequestUrl('search', getParameters)
@@ -62,7 +79,7 @@ export class ApiService {
 
 
     private _getSpecificRecipeAsObservable(recipeId): Observable<any> {
-        let keyValueList = [e.api.recipeIDParam, recipeId];
+        let keyValueList = [this._recipeIDParam, recipeId];
         let getParameters = this._createGetParameters(keyValueList);
         return this._getRequest(
             this._createRequestUrl('specific', getParameters)
@@ -73,7 +90,7 @@ export class ApiService {
     // parameter keyValueArray must alternate in this order:
     // [key1, key1's value,  key2, key2's value, and so on...]
     private _createGetParameters(keyValueArray) {
-        let getParameters = '?' + e.api.keyParamValuePair;
+        let getParameters = '?' + this._keyParamValuePair;
         for (let i = 0; i < keyValueArray.length; i += 2) {
             getParameters += ('&' + keyValueArray[i] + '=' + keyValueArray[i + 1]);
         }
@@ -82,7 +99,7 @@ export class ApiService {
 
 
     private _getRequest(url) {
-        return this._http.get(url, e.api.httpOptions);
+        return this._http.get(url, this._httpOptions);
     }
 
 
@@ -90,10 +107,10 @@ export class ApiService {
         let fullUrl = '';
 
         if (searchType === 'search') {
-            fullUrl = e.api.baseSearchUrl + getParameters;
+            fullUrl = this._baseSearchUrl + getParameters;
         }
         else if (searchType === 'specific') {
-            fullUrl = e.api.baseRecipeRequestUrl + getParameters;
+            fullUrl = this._baseRecipeRequestUrl + getParameters;
         }
         return fullUrl;
     }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,6 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
     production: false,
@@ -18,24 +17,4 @@ export const environment = {
         messagingSenderId: '870271143850'
     },
 
-    corsProxy: 'https://cors-anywhere.herokuapp.com/',
-
-    api: {
-        baseUrl: this.base_api.corsProxy + 'http://food2fork.com/api/',
-        baseSearchUrl: this.baseUrl + 'search',
-        baseRecipeRequestUrl: this.baseUrl + 'get',
-        keyParam: 'key',
-        apiKey: '4f5c0e8ca2c5a339763c1a9b0ce2374d',
-        keyParamValuePair: this.keyParam + '=' + this.apiKey,
-        queryParam: 'q',
-        recipeIDParam: 'rId',
-        pageParam: 'page',
-        httpOptions: {
-            headers: new HttpHeaders({
-                'Authorization': this.apiKey,
-                'Accept': ['text/html', 'application/json'],
-                'apikey': this.apiKey,
-            })
-        },
-    }
 };
